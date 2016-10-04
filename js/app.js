5592d7173834dbab7b151bde66e930222d91261a
Kye Norman-Gill <kye.normangill@tafensw.net.au>
2016-10-05 10:17:14 +1100

    Added Imperial algorithm and some conversions

--- js/app.js
+++ js/app.js
@@ -1,39 +1,71 @@
 /* 
-	══════════════════════ஜ۩۞۩ஜ═════════════════════
+	═══════════════════════════════════════════
 	Author: Kye Norman-Gill
 	Website: katsura.shianra.com
 	Version: 1.0
-	══════════════════════ஜ۩۞۩ஜ═════════════════════ 
+	═══════════════════════════════════════════ 
 */
 
 /*
-	@desc Weight and Height variables.
-*/
-var weight = 0.0;
-var height = 0.0;
-
-/*
-	@func calcBMI
-	@desc Calculates the body mass index from the height and weight given.
+	@func calcBMIMetric
+	@desc Calculates the body mass index from the height in meters and weight in kilograms given.
 	@param height
 	@param weight
 	@returns BMI
 */
-var calcBMI = function(height, weight) {
-	var BMI = 0.0;
-	var heightSquared = height * height;
-	
-	BMI = weight / heightSquared;
-	
-	return BMI;
+var calcBMIMetric = function(height, weight) {
+	return weight/(height*height);
+}
+
+/*
+	@func calcBMIImperial
+	@desc Calculates the body mass index from the height in inches and weight pounds given.
+	@param height
+	@param weight
+	@returns BMI
+*/
+var calcBMIImperial = function(height, weight){
+	return (weight*703)/(height*height)
+}
+
+/*
+	@func centimetersToMeters
+	@desc Converts centimeters to meters.
+	@param centimeters
+	@returns metres
+*/
+var centimetersToMeters = function(centimeters){
+	return centimeters/100;
+}
+
+/*
+	@func inchesToMeters
+	@desc Converts inches to meters.
+	@param inches
+	@returns metres
+*/
+var inchesToMeters = function(inches)
+{
+	return inches * 0.0254;
+}
+
+
+/*
+	@func inchesToMeters
+	@desc Converts inches to meters.
+	@param inches
+	@returns metres
+*/
+var poundsToKilograms = function(pounds){
+	return pounds/2.2046;
 }
 
 /*
 	@desc Gets both values and calulcates bmi and sends an alert box with the result
 */
 document.getElementById("calc").onclick = function() {
-	weight = document.getElementById("_weight").value;
-	height = document.getElementById("_height").value;
-	var result = calcBMI(height, weight);
+	var weight = document.getElementById("_weight").value;
+	var height = document.getElementById("_height").value;
+	var result = calcBMIImperial(height, weight);
 	window.alert(result);
 };
